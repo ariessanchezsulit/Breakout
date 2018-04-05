b96d81948eacef9ec18a0a8427b358567157ccd5
Aries Sanchez Sulit <ariessanchezsulit@gmail.com>
2018-04-05 21:04:33 +0800

    Breakout files

---
+++ Framework/Assets/Sandbox/Breakout/Scripts/Breakout.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+using UniRx;
+using UniRx.Triggers;
+
+using FullInspector;
+
+using uPromise;
+
+using Common;
+using Common.Extensions;
+using Common.Query;
+using Common.Utils;
+
+using Framework;
+
+namespace Sandbox.Shooting
+{
+    public class Breakout : SceneObject
+    {
+        
+    }
+}
